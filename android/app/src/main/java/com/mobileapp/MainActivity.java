6cad2ee1560b2fabda208ff1ffa19f05ed24b56e
Serg <redetection@gmail.com>
2016-11-05 13:50:05 +0700

    able to compile and see mainscreen on android

--- android/app/src/main/java/com/mobileapp/MainActivity.java
+++ android/app/src/main/java/com/mobileapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.mobileapp;
 
 import com.facebook.react.ReactActivity;
-import com.brentvatne.RCTVideo.ReactVideoPackage;
+import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
