8b19866c5f5f8ab272287bf0d7f4738110b8987c
Serg <redetection@gmail.com>
2016-11-05 18:57:41 +0700

    another fix for android after updating

--- android/app/src/main/java/com/mobileapp/MainActivity.java
+++ android/app/src/main/java/com/mobileapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.mobileapp;
 
 import com.facebook.react.ReactActivity;
-import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainActivity extends ReactActivity {
