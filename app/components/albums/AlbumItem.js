bddcb52225761d42c363e19217944da58d4bb4d2
Serg <redetection@gmail.com>
2017-03-05 18:00:33 +0700

    comments count on album, pluralise it and tracks count

--- app/components/albums/AlbumItem.js
+++ app/components/albums/AlbumItem.js
@@ -13,7 +13,13 @@ import {Actions} from 'react-native-router-flux';
 
 
 class AlbumItem extends Component {
+
   render() {
+    let tracksCount = this.props.repetition.tracks.length;
+    let commentsCount = this.props.repetition.tracks.reduce((accumulator, track) => {
+      return accumulator + track.comments.length;
+    }, 0);
+
     return (
       <TouchableHighlight onPress={ () => Actions.albumShow({ repetition: this.props.repetition }) } activeOpacity={ 100 } underlayColor="#ea4b54">
         <Image
@@ -23,7 +29,8 @@ class AlbumItem extends Component {
         >
         <View style={ styles.container }>
           <Text style={ styles.artistName }>{ this.props.repetition.title }</Text>
-          <Text style={ styles.artistSongs }>{ this.props.repetition.tracks.length } songs</Text>
+          <Text style={ styles.artistSongs }>{ tracksCount } { tracksCount == 1 ? "track" : "tracks"}</Text>
+          <Text style={ styles.artistSongs }>{ commentsCount } { commentsCount == 1 ? "comment" : "comments" }</Text>
         </View>
         </Image>
       </TouchableHighlight>
