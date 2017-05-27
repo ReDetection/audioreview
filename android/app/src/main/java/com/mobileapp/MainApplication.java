6e5b321ebe8967ba4ef43e285cfc2b2bf0038b27
Serg <redetection@gmail.com>
2017-05-27 22:23:46 +0700

    install another native filesystem module because it has upload support for android

--- android/app/src/main/java/com/mobileapp/MainApplication.java
+++ android/app/src/main/java/com/mobileapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobileapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.rnfs.RNFSPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNFetchBlobPackage(),
           new RNFSPackage(),
           new ReactVideoPackage(),
           new VectorIconsPackage(),
