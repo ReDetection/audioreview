db1ecdff92711c269f97989ef0c458d810ef5f6e
Serg <redetection@gmail.com>
2017-05-28 12:12:02 +0700

    missing config change

--- config.js
+++ config.js
@@ -3,4 +3,6 @@
 module.exports = {
     authURL: 'http://my.realm-auth-server.com:9080',
     modelURL: 'realm://object-server-url:9080/my-realm',
+    uploadBaseURL: 'http://localhost:8080/rehersal',
+    uploadedTracksBaseUrl: 'http://localhost:8080',
 };
