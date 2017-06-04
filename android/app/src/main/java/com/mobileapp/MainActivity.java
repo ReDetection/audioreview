e218387d6a36391fb752321b8f959f0820225489
Serg <redetection@gmail.com>
2017-06-04 17:21:42 +0700

    support for android sharing of audiofiles, patch android intent to pass hardcoded filter in the android implementation of linking support

--- android/app/src/main/java/com/mobileapp/MainActivity.java
+++ android/app/src/main/java/com/mobileapp/MainActivity.java
@@ -1,5 +1,9 @@
 package com.mobileapp;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,4 +16,21 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "mobileApp";
     }
+
+    @Override
+    public Intent getIntent() {
+        Intent origIntent = super.getIntent();
+        if (origIntent != null && Intent.ACTION_SEND.equals(origIntent.getAction())) {
+            return new Intent(Intent.ACTION_VIEW, this.uriFromClipData(origIntent.getClipData()));
+        }
+        return origIntent;
+    }
+
+    private Uri uriFromClipData(ClipData clip) {
+        if (clip != null && clip.getItemCount() > 0) {
+            return clip.getItemAt(0).getUri();
+        }
+        return null;
+    }
+
 }
