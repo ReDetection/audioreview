83b71a96f1d59fa517a92cca3cf4bba5e3bf07c9
Serg <redetection@gmail.com>
2017-06-04 23:12:37 +0700

    this slash seems to be important because I just concatenating without any check for slashes :)

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
     authURL: 'http://my.realm-auth-server.com:9080',
     modelURL: 'realm://object-server-url:9080/my-realm',
     uploadBaseURL: 'http://localhost:8080/rehersal',
-    uploadedTracksBaseUrl: 'http://localhost:8080',
+    uploadedTracksBaseUrl: 'http://localhost:8080/',
 };
