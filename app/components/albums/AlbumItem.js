65e7ff2d5b73ddcc0ee3e1017e948dc32b968595
Serg <redetection@gmail.com>
2017-07-02 12:01:12 +0700

    decouple album list item from album model

--- app/components/albums/AlbumItem.js
+++ app/components/albums/AlbumItem.js
@@ -2,13 +2,7 @@
 import React, {
   Component,
 } from 'react';
-import {
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-  Image,
-} from 'react-native';
+import AlbumItemRaw from './AlbumItemRaw';
 
 
 class AlbumItem extends Component {
@@ -20,46 +14,15 @@ class AlbumItem extends Component {
     }, 0);
 
     return (
-      <TouchableHighlight onPress={ this.props.onPress } activeOpacity={ 100 } underlayColor="#ea4b54">
-        <Image
-          style={ styles.artistBg }
-          resizeMode='cover'
-          source={{uri:  this.props.repetition.imageURL  }}
-        >
-        <View style={ styles.container }>
-          <Text style={ styles.artistName }>{ this.props.repetition.title }</Text>
-          <Text style={ styles.artistSongs }>{ tracksCount } { tracksCount == 1 ? "track" : "tracks"}</Text>
-          <Text style={ styles.artistSongs }>{ commentsCount } { commentsCount == 1 ? "comment" : "comments" }</Text>
-        </View>
-        </Image>
-      </TouchableHighlight>
+      <AlbumItemRaw onPress={ this.props.onPress } 
+                    title={this.props.repetition.title}
+                    subtitle={ tracksCount + (tracksCount == 1 ? " track" : " tracks") }
+                    description={ commentsCount + (commentsCount == 1 ? " comment" : " comments") }
+                    imageURL={this.props.repetition.imageURL}
+      />
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'rgba(0,0,0,0.7)',
-    paddingTop: 20,
-    paddingBottom: 60,
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-  artistName: {
-    color: "#FFF",
-    backgroundColor: 'transparent',
-    fontFamily: "Helvetica Neue",
-    fontWeight: "500",
-    fontSize: 18,
-    marginBottom: 5
-  },
-  artistSongs: {
-    color: "#CCC",
-    backgroundColor: 'transparent',
-    fontFamily: "Helvetica Neue",
-    fontWeight: "300",
-    fontSize: 14
-  },
-});
+}
 
 module.exports = AlbumItem;
